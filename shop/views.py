e432e09582c566025e06cff6cee2d32e40cadb48
Karen Samad <KarenLSamad@gmail.com>
2024-06-26 13:58:20 +0000

    Ammendments to JS scripts.

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.contrib import messages
+from django.db.models import Q
 from products.models import Category, Product
 from events.models import Event 
 
@@ -11,7 +13,24 @@ def shop(request):
     categories = Category.objects.all()
     products = Product.objects.all()
     events = Event.objects.all()
+    query = None
 
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter a search")
+                return redirect(reverse('shop'))
+            # product queries for name, descritpion and category
+            product_queries = (
+            Q(name__icontains=query) | 
+            Q(description__icontains=query) | 
+            Q(category__name__icontains=query)
+            )
+            # event query for name and description (as no category)
+            event_queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(product_queries)
+            events = events.filter(event_queries)
 
     combined_list = []
     for product in products:
@@ -30,7 +49,8 @@ def shop(request):
 
     context = {
         'categories': categories,
-        'combined_list': combined_list_sorted
+        'combined_list': combined_list_sorted,
+        'search_term': query,
     }
 
     return render(request, 'shop/shop.html', context)
