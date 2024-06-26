2cf427a5abb96d09ea41d72f80cac0e383c8ec5e
Karen Samad <KarenLSamad@gmail.com>
2024-06-26 12:41:54 +0000

    view for events and products all on main shop page

--- shop/views.py
+++ shop/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
 from products.models import Category, Product
+from events.models import Event 
 
 
 
 def shop(request):
     """ View function to render the shop page showing all categories of products and events
-    plus all products """
+    plus all individual products and events """
 
     categories = Category.objects.all()
     products = Product.objects.all()
+    events = Event.objects.all()
+
+
+    combined_list = []
+    for product in products:
+        combined_list.append({
+            'item': product,
+            'item_type': 'Product',
+        })
+    for event in events:
+        combined_list.append({
+            'item': event,
+            'item_type': 'Event',
+        })
+
+    # alphabetical sorting
+    combined_list_sorted = sorted(combined_list, key=lambda x: x['item'].friendly_name.lower())
 
     context = {
         'categories': categories,
-        'products': products,
+        'combined_list': combined_list_sorted
     }
+
     return render(request, 'shop/shop.html', context)
