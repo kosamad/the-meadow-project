9de4129826fbd83707b219380351f54f20a88ad3
Karen Samad <KarenLSamad@gmail.com>
2024-07-02 16:08:20 +0000

    Mid bug fix to render bag and access appropriate data.

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,15 +3,41 @@
 from decimal import Decimal
 from django.conf import settings
 
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def bag_contents(request):
 
     bag_items = []
-    total = 0
+    total = Decimal('0.00')
     product_count = 0
+    # Get shopping bag (or empty dictionary if absent)
+    bag = request.session.get('bag',{})
+
+    for item_id, item_data in bag.items():
+        print(f"Item ID: {item_id}")
+        item_components = item_id.split('_')
+        print(f"Item Components: {item_components}")
+        product_id = item_components[0]
+        size = item_components[1]
+        optional_card_message = item_components[2]
+        note_to_seller = item_components[3]
+        selected_size_price = item_data['selected_size_price']
+        quantity = item_data['quantity']
+
+        product = get_object_or_404(Product, pk=product_id)
+        total += quantity * Decimal(selected_size_price)
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+            'selected_size_price': selected_size_price,
+        }) 
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/ 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from .models import Product
 from .forms import ProductOrderForm
 
 
+
 def product_detail(request, product_id):
     """ A view to show the product details for an individual item """
     product = get_object_or_404(Product, pk=product_id)
@@ -19,6 +20,8 @@ def add_to_bag(request, product_id):
     """ A view to handle adding a product to the shopping bag """
     product = get_object_or_404(Product, pk=product_id)
     redirect_url = request.POST.get('redirect_url')
+    # getting bag variable in the session or create one.
+    bag = request.session.get('bag',{})
 
     if request.method == 'POST':        
         form = ProductOrderForm(request.POST)
@@ -27,28 +30,28 @@ def add_to_bag(request, product_id):
             optional_card_message = form.cleaned_data.get('optional_card_message', '')
             note_to_seller = form.cleaned_data.get('note_to_seller', '')
             quantity = form.cleaned_data['quantity']
-            selected_size_price = str(form.cleaned_data['selected_size_price']) # stored as a string
+            selected_size_price = form.cleaned_data['selected_size_price']
             product_id = form.cleaned_data['product_id']           
 
             # Unique identifyer for each product.
             item_id = f"{product_id}_{size}_{optional_card_message}_{note_to_seller}"
-
-            # getting bag variable in the session or create one.
-            bag = request.session.get('bag',{})
-
+           
             if item_id in bag:
                 bag[item_id]['quantity'] += quantity
             else:
-                bag[item_id] = {
-                    'quantity': quantity,
-                    'size': size,                    
-                    'optional_card_message': optional_card_message,
-                    'note_to_seller': note_to_seller,
-                    'selected_size_price': selected_size_price
+                 bag[item_id] = {
+                'quantity': quantity,
+                'size': size,
+                'optional_card_message': optional_card_message,
+                'note_to_seller': note_to_seller,
+                'selected_size_price': selected_size_price,
+                'product_id': product_id,
                 }
 
             # Store the updated bag back into the session
-            request.session['bag'] = bag                
+            request.session['bag'] = bag
+
+            print.session['bag']                    
 
             return redirect(redirect_url)
     else:
