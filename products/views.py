f8007dd6c08ca13c41839f2687407eed4c40d42d
Karen Samad <KarenLSamad@gmail.com>
2024-07-02 13:33:46 +0000

    split function into product detail and product, add to bag

--- products/views.py
+++ products/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 from .forms import ProductOrderForm
 
 
 def product_detail(request, product_id):
     """ A view to show the product details for an individual item """
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductOrderForm()
 
+    context = {
+        'product': product,
+        'form': form,
+    }
+    return render(request, 'products/product_detail.html', context)
+
+
+def add_to_bag(request, product_id):
+    """ A view to handle adding a product to the shopping bag """
     product = get_object_or_404(Product, pk=product_id)
 
-    # Prduct order form
     if request.method == 'POST':
         form = ProductOrderForm(request.POST)
         if form.is_valid():
@@ -16,17 +26,33 @@ def product_detail(request, product_id):
             date = form.cleaned_data['date']
             delivery_option = form.cleaned_data['delivery_option']
             optional_card_message = form.cleaned_data.get('optional_card_message', '')
-            note_to_seller = form.cleaned_data.get('not_to_seller', '')
+            note_to_seller = form.cleaned_data.get('note_to_seller', '')
             quantity = form.cleaned_data['quantity']
             selected_size_price = form.cleaned_data['selected_size_price']
             product_id = form.cleaned_data['product_id']
-            return redirect('success')
-        else:
-            form = ProductOrderForm()
+            redirect_url = form.cleaned_data['redirect_url']
 
+            item_id = str(product_id)
+
+            # getting bag variable in the session or create one.
+            bag = request.session.get('bag',{})
+
+            if item_id in bag:
+                bag[item_id] += quantity
+            else:
+                bag[item_id] = quantity
+
+            # Store the updated bag back into the session
+            request.session['bag'] = bag
+
+            print(request.session['bag'])          
+
+            return redirect(redirect_url)
+    else:
+        form = ProductOrderForm()
 
     context = {
         'product': product,
-        'form': ProductOrderForm(),
+        'form': form,
     }
     return render(request, 'products/product_detail.html', context)
