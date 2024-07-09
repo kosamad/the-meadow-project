f54fdc8be35e8eb7d21e37c4c5295955d993badd
Karen Samad <KarenLSamad@gmail.com>
2024-07-09 16:07:01 +0000

    Default price (M) runs with product and select box to choose size.

--- products/views.py
+++ products/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product, Event
+from .models import Product, Event, ProductVariant
 
 
 def product_detail(request, product_uuid):
     """ A view to show the product details for an individual item """
 
     product = get_object_or_404(Product, id=product_uuid) # id=product_id I am using UUID
+    variants = ProductVariant.objects.filter(product=product)
+    default_variant = variants.filter(size='M').first() or variants.first()
+    default_price = default_variant.price if default_variant else None
+
     context = {
-        'product': product,        
+        'product': product,
+        'variants': variants,
+        'default_price': default_price,
+        'default_variant_id': default_variant.id if default_variant else None,
     }
     return render(request, 'products/product_detail.html', context)
 
