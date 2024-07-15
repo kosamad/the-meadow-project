27af9e24d97d1710686f2857369b4545412545fd
Karen Samad <KarenLSamad@gmail.com>
2024-07-15 14:35:04 +0000

    Basic checkout page set up with form ready to render.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.contrib import messages
+
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product, Event, ProductVariant
 
 # Create your views here.
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('shop'))
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+    return render(request, template, context)
--- the_meadow_project/urls.py
+++ the_meadow_project/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),       
     path('shop/', include('shop.urls')),
-    path('bag/', include('bag.urls')),       
+    path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
