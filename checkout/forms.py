0205c7c62ccb831c3a143a487482ea49d8f3d823
Karen Samad <KarenLSamad@gmail.com>
2024-07-17 07:59:40 +0000

    Delivery method doesn't work with stripe formating. In progress.

--- checkout/forms.py
+++ checkout/forms.py
@@ -34,13 +34,17 @@ class OrderForm(forms.ModelForm):
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
-        for field in self.fields:            
+        for field in self.fields:         
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
             else:
                 placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            # Ensure 'delivery_method' has no class ad
+            if field != 'delivery_method': 
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            else:                
+                self.fields[field].widget.attrs.pop('class', None)
             self.fields[field].label = False
 
     
