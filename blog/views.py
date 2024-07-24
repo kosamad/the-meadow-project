bbb6040d0c721f1763e599e808202ba048a818ba
Karen Samad <KarenLSamad@gmail.com>
2024-07-24 13:43:52 +0000

    Search bar with styling. Renders correct blog results (posts).

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import Post
+
 
 
 # Create your views here.
@@ -9,16 +11,34 @@ def all_posts(request):
     """ A view to return the blog page with all blog items """
     # Gather data
     posts = Post.objects.all()
+
+    # Search requests
+    query = ''  
+    if 'q' in request.GET:
+        query = request.GET['q']
+        # if the query is blank = an error message
+        if not query:
+            messages.error(request, "You didn't enter a search")
+            return redirect(reverse('posts'))
+
+        # product and event queries for name, descritpion and category
+        post_queries = (
+        Q(title__icontains=query) | 
+        Q(body__icontains=query)              
+        )
+        posts = posts.filter(post_queries)
+
     # pagination to limit how many posts per page
     paginator = Paginator(posts, 6)
-
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginator.get_page(page_number)  
+               
 
     context = {
         'posts': posts,
         'page_number': page_number,
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'search_term': query,
         }
 
     return render(request, 'blog/posts.html', context)
