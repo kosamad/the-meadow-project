8ac340cd0ed5dc1c998192445e6384592e3107ca
Karen Samad <KarenLSamad@gmail.com>
2024-07-24 13:17:38 +0000

    Blog pagination and styling.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 from .models import Post
 
 
@@ -9,16 +9,17 @@ def all_posts(request):
     """ A view to return the blog page with all blog items """
     # Gather data
     posts = Post.objects.all()
+    # pagination to limit how many posts per page
+    paginator = Paginator(posts, 6)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
         'posts': posts,
-    }
-
-
-
-
-
-
+        'page_number': page_number,
+        'page_obj': page_obj
+        }
 
     return render(request, 'blog/posts.html', context)
 
