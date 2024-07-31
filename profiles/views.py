58639cd00523c4535ae4633c7d2b09f5e69ff96e
Karen Samad <KarenLSamad@gmail.com>
2024-07-31 12:18:56 +0000

    Formatting for allauth and basic profile views etc ready.

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
+from django.contrib import messages
 
 # Create your views here.
 
 def profile(request):
     '''Display the Users profile'''
 
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance = profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    form = UserProfileForm(instance=profile) # user default info
+    orders = profile.orders.all() # get users orders
+
+    # get users username and eamil for display
+    username = profile.user.username 
+    email = profile.user.email
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form':form,
+        'orders':orders,
+        'username': username,
+        'email': email,        
+    }
 
     return render (request, template, context)
