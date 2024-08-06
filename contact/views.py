3ee7be8d319f0b24313551f2315ea8e1ea73f83b
Karen Samad <KarenLSamad@gmail.com>
2024-08-06 12:47:54 +0000

    Emails from contact from send to gmail.

--- contact/views.py
+++ contact/views.py
@@ -1,32 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
 
 # Create your views here.
 
 
 
 
-
+# Contact form code adapted from Codemy.com YouTube video series https://www.youtube.com/watch?v=xNqnHmXIuzU
 def contact(request):
     """ A view to return the index page """
 
     if request.method == 'POST':
-        message_name = request.POST('message_name')
-        message_email = request.POST['message_email']
-        message = request.POST['message']
+        message_name = request.POST.get('message-name')
+        message_email = request.POST.get('message-email')
+        message = request.POST.get('message')
 
-        context = {
-            'message_name': message_name,
-            'message_email': message_email
-        }
+        #  Validating form and ensures all parts are presnt
+        if not message_name or not message_email or not message:
+            messages.error(request, 'Please fill in all fields.')
+            return render(request, 'contact/contact.html')
 
-        template = 'contact/contact.html'
-       
-        return render(request, template, context)      
+        # send email
+        send_mail(
+            f'Contact form message from {message_name}',  # subject
+            message,  # message
+            message_email,  # from email
+            ['themeadowproj@gmail.com'],  # to email
+        )
 
-    
-    else:
-        return render(request, 'contact/contact.html')
-
-
+        messages.success(request, 'Your email has been sent successfully!')
+        return redirect('contact')        
 
     return render(request, 'contact/contact.html')
--- the_meadow_project/settings.py
+++ the_meadow_project/settings.py
@@ -226,3 +226,15 @@ else:
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+
+
+# Email Settings for contact form
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER','')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD','')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL','')
