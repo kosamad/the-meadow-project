f7fc645949febd83d2c5f055bbd8000bbc57be27
Karen Samad <KarenLSamad@gmail.com>
2024-08-14 13:28:08 +0000

    Validation to ensure body is not whitespace only.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from products.models import Product, Event
 from django.urls import reverse
 from datetime import datetime, date
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -13,7 +14,12 @@ class Post(models.Model):
     alt_text = models.TextField(default="", null=False, blank=False)
     product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, null=True, blank=True, on_delete=models.CASCADE)
-    body = models.TextField(null=False, blank=False)   
+    body = models.TextField(null=False, blank=False)
+
+    def clean(self):
+        super().clean()
+        if not self.body or self.body.strip() == '':
+            raise ValidationError({'body': 'Body text cannot be empty or contain only whitespace.'}) 
 
     def __str__(self):
         """
