5d30370d1fef7a451b985d0df48cb27d5b60c01d
Karen Samad <KarenLSamad@gmail.com>
2024-08-14 13:27:53 +0000

    Formating of form code.

--- products/forms.py
+++ products/forms.py
@@ -26,8 +26,7 @@ class ProductForm(forms.ModelForm):
         self.fields['name'].help_text = 'This should be set like this example ada_bouquet for Ada Bouquet friendly name'
         self.fields['price'].help_text = 'Set the price of the product. Set this to the price of a medium size.'
         self.fields['alt_text'].help_text = 'Describe the image'
-        self.fields['is_active'].help_text = 'Checked if the product is avaliable'    
-
+        self.fields['is_active'].help_text = 'Checked if the product is avaliable'
 
 
 class ProductVariantForm(forms.ModelForm):
