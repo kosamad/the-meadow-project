65ea29cb8527cb48b8019ab8b2420e8428409478
Karen Samad <KarenLSamad@gmail.com>
2024-09-03 20:43:24 +0000

    test for deployment

--- the_meadow_project/settings.py
+++ the_meadow_project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['8000-kosamad-themeadowprojec-6rk3byj73lq.ws.codeinstitute-ide.net',
-'the-meadow-project-c89db5579746.herokuapp.com',
+'the-meadow-project-800f5f20f1b1.herokuapp.com',
 ]
 
 
@@ -236,7 +236,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Where static files are. note no static route as thi interfers with settings on Amazon Web services 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
