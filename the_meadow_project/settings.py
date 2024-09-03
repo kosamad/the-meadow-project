3b6cfa70b7c10e2fe3a7967e138c07b4353e3e08
Karen Samad <KarenLSamad@gmail.com>
2024-09-03 15:23:39 +0000

    Formatting.

--- the_meadow_project/settings.py
+++ the_meadow_project/settings.py
@@ -234,7 +234,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 # Where static files are. note no static route as thi interfers with settings on Amazon Web services 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
