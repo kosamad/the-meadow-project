120c6a87ff8e21633a3ba1fce9eba51bf80d2c62
Karen Samad <KarenLSamad@gmail.com>
2024-09-03 08:37:11 +0000

    Correction to urls.py to us id not UUID.

--- bag/urls.py
+++ bag/urls.py
@@ -3,12 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    # path('add/<uuid:item_id>/', views.add_to_bag, name='add_to_bag'), 
-    path('add_product_to_bag/<uuid:item_id>/', views.add_product_to_bag, name='add_product_to_bag'),
-    path('add_event_to_bag/<uuid:item_id>/', views.add_event_to_bag, name='add_event_to_bag'),
-    path('update_card_message/<uuid:item_id>/', views.update_card_message, name='update_card_message'),
-    path('update_note_to_host/<uuid:item_id>/', views.update_note_to_host, name='update_note_to_host'),
-    path('update_note_to_seller/<uuid:item_id>/', views.update_note_to_seller, name='update_note_to_seller'),
-    path('update_quantity/<uuid:item_id>/', views.update_quantity, name='update_quantity'),
-    path('remove_item/<uuid:item_id>/', views.remove_item, name='remove_item'),       
+    path('add_product_to_bag/<int:item_id>/', views.add_product_to_bag, name='add_product_to_bag'),
+    path('add_event_to_bag/<int:item_id>/', views.add_event_to_bag, name='add_event_to_bag'),
+    path('update_card_message/<int:item_id>/', views.update_card_message, name='update_card_message'),
+    path('update_note_to_host/<int:item_id>/', views.update_note_to_host, name='update_note_to_host'),
+    path('update_note_to_seller/<int:item_id>/', views.update_note_to_seller, name='update_note_to_seller'),
+    path('update_quantity/<int:item_id>/', views.update_quantity, name='update_quantity'),
+    path('remove_item/<int:item_id>/', views.remove_item, name='remove_item'),
 ]
--- products/urls.py
+++ products/urls.py
@@ -2,17 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product/<uuid:product_uuid>/', views.product_detail, name='product_detail'),
-    path('event/<uuid:event_uuid>/', views.event_detail, name='event_detail'),
+    path('product/<int:id>/', views.product_detail, name='product_detail'),
+    path('event/<int:id>/', views.event_detail, name='event_detail'),
     path('add_product/', views.add_product, name='add_product'),
     path('add_event/', views.add_event, name='add_event'),
-    path('add_variant/<uuid:product_uuid>/', views.add_product_variant, name='add_product_variant'),
-    path('edit_product/<uuid:product_uuid>/', views.edit_product, name='edit_product'),
-    path('delete_product/<uuid:product_uuid>/', views.delete_product, name='delete_product'),
+    path('add_variant/<int:product_id>/', views.add_product_variant, name='add_product_variant'),
+    path('edit_product/<int:id>/', views.edit_product, name='edit_product'),
+    path('delete_product/<int:id>/', views.delete_product, name='delete_product'),
     path('delete_product_variant/<int:variant_id>/', views.delete_product_variant, name='delete_product_variant'),
     path('edit_product_variant/<int:variant_id>/', views.edit_product_variant, name='edit_product_variant'),
-    path('edit_event/<uuid:event_uuid>/', views.edit_event, name='edit_event'),
-    path('delete_event/<uuid:event_uuid>/', views.delete_event, name='delete_event'),
+    path('edit_event/<int:id>/', views.edit_event, name='edit_event'),
+    path('delete_event/<int:id>/', views.delete_event, name='delete_event'),
 ]
 
 
