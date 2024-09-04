31e5aa20d864eb03503bf57549a81ceaf56d8cf4
Karen Samad <KarenLSamad@gmail.com>
2024-09-04 14:01:35 +0000

    Deploy to collect static.

--- the_meadow_project/settings.py
+++ the_meadow_project/settings.py
@@ -235,7 +235,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # Where static files are. note no static route as thi interfers with settings on Amazon Web services
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
